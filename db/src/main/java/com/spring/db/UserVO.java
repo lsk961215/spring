b4d5bbc036075ea458d9697ad9652af8b20dd47a
이상기 <lsk961215@gmail.com>
2023-11-03 13:42:23 +0900

    no message

---
+++ db/src/main/java/com/spring/db/UserVO.java
@@ -0,0 +1,61 @@
+package com.spring.db;
+
+public class UserVO {
+	private int userNumber;
+	private String userId;
+	private String userPw;
+	private String userName;
+	private String userEmail;
+	private String userPhone;
+	private String userBirth;
+	private String userReg;
+	
+	public int getUserNumber() {
+		return userNumber;
+	}
+	public void setUserNumber(int userNumber) {
+		this.userNumber = userNumber;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getUserPw() {
+		return userPw;
+	}
+	public void setUserPw(String userPw) {
+		this.userPw = userPw;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPhone() {
+		return userPhone;
+	}
+	public void setUserPhone(String userPhone) {
+		this.userPhone = userPhone;
+	}
+	public String getUserBirth() {
+		return userBirth;
+	}
+	public void setUserBirth(String userBirth) {
+		this.userBirth = userBirth;
+	}
+	public String getUserReg() {
+		return userReg;
+	}
+	public void setUserReg(String userReg) {
+		this.userReg = userReg;
+	}
+}
